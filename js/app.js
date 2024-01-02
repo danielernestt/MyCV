f8006bc683661b6daa7992ea5d8422dfe015ca74
danielernestt <154586401+danielernestt@users.noreply.github.com>
2024-01-02 21:30:14 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -7,3 +7,16 @@ ScrollSmoother.create({
 	wrapper: '.wrapper',
 	content: '.content'
 })
+
+
+
+/* ================= MOBILE NAV ========================= */
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.mobile-nav');
+
+mobileNavButton.addEventListener('click', function () {
+  mobileNavIcon.classList.toggle('active');
+  mobileNav.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
+});
